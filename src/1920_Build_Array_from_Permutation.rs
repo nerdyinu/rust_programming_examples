3fd8daf687adada666042dfe94f89b7e8582c93a
inujung <inust33@cluml.com>
2025-03-25 16:23:30 +0900

    1920. Build Array from Permutation

---
+++ src/1920_Build_Array_from_Permutation.rs
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
+        let mut ans = vec![0;nums.len()];
+        for i in 0..nums.len() as usize{
+            ans[i] = nums[nums[i] as usize];
+        }
+        return ans
+    }
+}
