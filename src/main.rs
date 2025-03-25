1afc6a35108fc1740032ef434d6cfa8879feb417
inujung <inust33@cluml.com>
2025-03-25 17:55:13 +0900

    mod filenames

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::path;
+
+#[path = "1920_Build_Array_from_Permutation.rs"]
+mod Build_Array_from_Permutation;
+#[path = "1929_Concatenation.rs"]
+mod Concatenation;
+#[path = "1672_Richest_Customer_Wealth.rs"]
+mod Richest_Customer_Wealth;
+#[path = "1480_Running_Sum_of_1d_array.rs"]
+mod Running_Sum_of_1d_Array;
 fn main() {
     println!("Hello, world!");
 }
