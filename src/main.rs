67a684b66b67fc97b2dc7a7c00f4ef72a7de9645
inujung <inust33@cluml.com>
2025-03-25 16:35:40 +0900

    1512. number of good pairs
    1512번 해답

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod prob_1470_shuffle_the_array;
 mod prob_1480_running_sum_of_1d_array;
+mod prob_1512_number_of_good_pairs;
 mod prob_1672_richest_customer_wealth;
 mod prob_1920_build_array_from_permutation;
 mod prob_1929_concatenation_of_array;
