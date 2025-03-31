7801f3ab04f9aac54f9fe0876af996a56e094eb2
inujung <inust33@cluml.com>
2025-03-31 16:43:58 +0900

    2114. Maximum Number of Words Found in Sentences
    2114번 해답
    Closes #28

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod prob_1512_number_of_good_pairs;
 mod prob_1672_richest_customer_wealth;
 mod prob_1920_build_array_from_permutation;
 mod prob_1929_concatenation_of_array;
+mod prob_2114_maximum_number_of_words_found_in_sentences;
 mod prob_2160_minimum_sum_of_four_digit_number;
 mod prob_771_jewels_and_stones;
 fn main() {
